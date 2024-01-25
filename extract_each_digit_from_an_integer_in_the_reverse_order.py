e8a882bef2b04926b44738ef4ff7de1378a904eb
RheoCents <150512072+RheoCents@users.noreply.github.com>
2024-01-25 15:56:33 +0800

    Added new line to make print better

--- extract_each_digit_from_an_integer_in_the_reverse_order.py
+++ extract_each_digit_from_an_integer_in_the_reverse_order.py
@@ -1,6 +1,6 @@
 print('This code will reverse your inputted number and add spaces in between')
 user_input_number = int(input('Now can you give me a number? '))
-print('Your reversed number is:')
+print('\nYour reversed number is:')
 
 for i in range (-1,((len(str(user_input_number))*-1)-1),-1):
     print(str(user_input_number)[i], end=" ")
