2e6ed4d49f327b6c4eaf9b455374972659cd2b7c
unknown <centinaraphael0157@gmail.com>
2024-01-25 15:55:13 +0800

    Code worked and print done

--- extract_each_digit_from_an_integer_in_the_reverse_order.py
+++ extract_each_digit_from_an_integer_in_the_reverse_order.py
@@ -1,5 +1,6 @@
 print('This code will reverse your inputted number and add spaces in between')
 user_input_number = int(input('Now can you give me a number? '))
+print('Your reversed number is:')
 
 for i in range (-1,((len(str(user_input_number))*-1)-1),-1):
-    print('Your reversed number is:',str(user_input_number)[i], end=" ")
+    print(str(user_input_number)[i], end=" ")
